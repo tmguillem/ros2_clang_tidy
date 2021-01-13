93b448817768e1d5962d3eee3c4ddda287769f8e
Guillem <Guillem.Torrente@sony.com>
2021-01-13 13:08:23 +0900

    Run clang tidy

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,12 +14,9 @@ rsync -aq --remove-source-files . ws/src/$project_name --exclude ws
 
 cd ws
 
-# Set GUI_FLAG to False to be able to run the simulator in the container. TODO: pass this as an argument?
-sed -i 's/GUI_FLAG = True/GUI_FLAG = False/' src/$project_name/simulator/bullet_simulation/bullet_simulation/full_simulator.py
-
 # Compile and source workspace packages
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
-cd src/$project_name
-git status
+cd src
+for file in $(find -regex '.*\.\(cpp\)$'); do echo $file && clang-tidy -p ../build -header-filter='.*' -fix -fix-errors $file; done
