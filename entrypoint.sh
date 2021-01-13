c66a16ab7f0dddab2d59b53cfbd2bfe3eaefc80a
Guillem <Guillem.Torrente@sony.com>
2021-01-13 12:31:35 +0900

    Initial test

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+env
+
+mkdir -p $GITHUB_WORKSPACE/ws/src
+cd $GITHUB_WORKSPACE
+
+# Move all files inside ws/src
+rsync -aq --remove-source-files . ws/src --exclude ws
+
+cd ws
+
+# Set GUI_FLAG to False to be able to run the simulator in the container. TODO: pass this as an argument?
+sed -i 's/GUI_FLAG = True/GUI_FLAG = False/' src/simulator/bullet_simulation/bullet_simulation/full_simulator.py
+
+# Compile and source workspace packages
+source "/opt/ros/$ROS_DISTRO/setup.bash"
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
