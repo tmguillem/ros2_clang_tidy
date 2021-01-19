be11e3f79a4cc7aaa5f8ace6feff96c6a7594233
Guillem <Guillem.Torrente@sony.com>
2021-01-19 11:34:11 +0900

    Update clang-tidy script

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,10 +16,14 @@ cd ws
 
 # Compile and source workspace packages
 source "/opt/ros/$ROS_DISTRO/setup.bash"
-colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBUILD_TESTING=OFF
 
 cd src/$project_name
-for file in $(find -regex '.*\.\(cpp\)$'); do echo $file && clang-tidy -p ../../build -header-filter='.*' -fix -fix-errors $file; done
+
+# Get list of all cpp files to analize
+cpp_list=$(find . -regex '.*\.\(cpp\)')
+
+for file in "${cpp_list}"; do echo $file && clang-tidy -p ../../build -header-filter='.*' -fix $file; done
 
 if [[ `git status --porcelain --untracked-files=no` ]]; then
     echo "Fixes in files required. Exiting"
