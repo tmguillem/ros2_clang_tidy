0a75d4ef9bce4ac9b145157e254266a9a0c5d214
Guillem <Guillem.Torrente@sony.com>
2021-06-15 13:07:51 +0900

    Wip

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,6 +19,8 @@ source "/opt/ros/$ROS_DISTRO/setup.bash"
 colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBUILD_TESTING=OFF
 
 cd src/$project_name
+ls
+mv /run-clang-tidy.py .
 
 all_passed=true
 
