1182bed0b31064590bf5976f12ddd897f51238d9
Guillem <Guillem.Torrente@sony.com>
2021-09-06 18:58:52 +0900

    Move things around

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,21 +14,6 @@ rsync -aq --remove-source-files . ws/src/"$project_name" --exclude ws
 
 cd ws
 
-# Compile and source workspace packages
-source "/opt/ros/$ROS_DISTRO/setup.bash"
-colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBUILD_TESTING=OFF
-
-cd src/"$project_name"
-mv /run-clang-tidy.py .
-
-# Read list of ignored directories
-ignored_paths=()
-while IFS= read -r line; do if [[ ${line::1} != "#" ]] ; then ignored_paths+=("$line"); fi; done < .clang-tidy-ignore
-echo "These directories will be ignored:"
-for path in "${ignored_paths[@]}"; do echo "$path"; done
-
-all_passed=true
-
 # Determine the version of clang-tidy:
 if [ "$INPUT_VERSION" == 10 ] ; then
   clang_binary="clang-tidy"
@@ -43,6 +28,21 @@ else
   printf "Expected version 10 or 12 but got %s" "$INPUT_VERISON" >&2  # write error message to stderr
   exit 1
 fi
+
+# Compile and source workspace packages
+source "/opt/ros/$ROS_DISTRO/setup.bash"
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DBUILD_TESTING=OFF
+
+cd src/"$project_name"
+mv /run-clang-tidy.py .
+
+# Read list of ignored directories
+ignored_paths=()
+while IFS= read -r line; do if [[ ${line::1} != "#" ]] ; then ignored_paths+=("$line"); fi; done < .clang-tidy-ignore
+echo "These directories will be ignored:"
+for path in "${ignored_paths[@]}"; do echo "$path"; done
+
+all_passed=true
 
 echo "Running script"
 time python3 run-clang-tidy.py -p ../../build \
